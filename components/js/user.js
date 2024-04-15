5f79017b07fd0b3849f0397eece8a239bd89529b
MariaMushtaq-12 <117312073+MariaMushtaq-12@users.noreply.github.com>
2024-04-15 13:59:22 +0500

    Update user.js

--- components/js/user.js
+++ components/js/user.js
@@ -12,4 +12,31 @@ document.addEventListener('DOMContentLoaded', function () {
             zoom: 2
         })
     });
+
+    // Function to add a layer to the map
+    function addLayer(name, source) {
+        const layer = new ol.layer.Image({
+            title: name,
+            source: new ol.source.ImageStatic({
+                url: source,
+                projection: 'EPSG:3857', // Assuming the projection of the uploaded files is EPSG:3857 (Web Mercator)
+                imageExtent: [-20037508.34, -20037508.34, 20037508.34, 20037508.34] // Full extent of EPSG:3857
+            })
+        });
+        map.addLayer(layer);
+    }
+
+    // Add layers for testing
+    addLayer("Open Street Map", "https://a.tile.openstreetmap.org/")
+    addLayer("ESRI World Imagery", "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}");
+
+    // Add Layers widget
+    const layerSwitcher = new ol.control.LayerSwitcher({
+        reverse: true,
+        groupSelectStyle: 'children',
+        showOpacity: true,
+        showLabels: true,
+        collapsed: false
+    });
+    map.addControl(layerSwitcher);
 });
